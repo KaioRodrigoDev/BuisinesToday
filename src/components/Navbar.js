80cdd9e8c14f0bb1f79ce869da2164ae38362da1
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-04-02 02:12:20 -0300

    Mudanças de Link para vercel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,10 +20,10 @@ export default function Navbar() {
           >
             <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
               <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="/">
+                <Link href="/">
                   <span className="sr-only">Workflow</span>
                   <GlobeIcon className="h-8 w-auto sm:h-10 text-principal" />
-                </a>
+                </Link>
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
@@ -34,20 +34,20 @@ export default function Navbar() {
             </div>
             <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
               {navigation.map(item => (
-                <a
+                <Link
                   key={item.name}
                   href={item.href}
                   className="font-bold text-gray-500 hover:text-gray-900"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
-              <a
+              <Link
                 href="#"
                 className="font-bold text-principal hover:text-principalh"
               >
                 Orçamento
-              </a>
+              </Link>
             </div>
           </nav>
         </div>
