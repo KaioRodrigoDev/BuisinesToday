5a61faf6c68a900162cd6fc2e2910772a10d02c0
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-04-02 18:47:55 -0300

    Link na imagem do Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -56,7 +56,12 @@ export default function Navbar() {
           <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
             <div className="px-5 pt-4 flex items-center justify-between">
               <div>
-                <GlobeIcon className="h-8 w-auto sm:h-10 text-principal" />
+                <Link href="/">
+                  <a>
+                    <span className="sr-only">Workflow</span>
+                    <GlobeIcon className="h-8 w-auto sm:h-10 text-principal" />
+                  </a>
+                </Link>
               </div>
               <div className="-mr-2">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
