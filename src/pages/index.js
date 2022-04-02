3b1e6172ce3f157cca97f0085c2a4f5a5af570b7
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-04-02 02:43:15 -0300

    Tema Dark

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative dark:bg-[#17202C] overflow-hidden">
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="relative z-10 pb-8 dark:bg-[#17202C] sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
             fill="currentColor"
@@ -12,7 +12,10 @@ export default function Home() {
             preserveAspectRatio="none"
             aria-hidden="true"
           >
-            <polygon points="50,0 100,0 50,100 0,100" />
+            <polygon
+              className="dark:text-[#17202C]"
+              points="50,0 100,0 50,100 0,100"
+            />
           </svg>
 
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
