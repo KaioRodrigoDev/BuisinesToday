52ab698bac8d7e4c37a0f212b15feb175d0d4d7b
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-04-02 18:37:41 -0300

    Navbar funcional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,52 +52,35 @@ export default function Navbar() {
           </nav>
         </div>
 
-        <Transition
-          as={Fragment}
-          enter="duration-150 ease-in"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-1 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-          >
-            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-              <div className="px-5 pt-4 flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt=""
-                  />
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Close main menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
+        <Popover.Panel className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right ">
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <GlobeIcon className="h-8 w-auto sm:h-10 text-principal" />
               </div>
-              <div className="px-2 pt-2 pb-3 space-y-1">
-                {navigation.map(item => (
-                  <Link key={item.name} href={item.href}>
-                    <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
-                      {item.name}
-                    </a>
-                  </Link>
-                ))}
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close main menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
               </div>
-              <Link href="/">
-                <a className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
-                  Log in
-                </a>
-              </Link>
             </div>
-          </Popover.Panel>
-        </Transition>
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map(item => (
+                <Link key={item.name} href={item.href}>
+                  <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
+                    {item.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+            <Link href="/">
+              <a className="block w-full px-5 py-3 text-center font-medium text-principal bg-gray-50 hover:bg-gray-100">
+                Orçamentos
+              </a>
+            </Link>
+          </div>
+        </Popover.Panel>
       </Popover>
     </div>
   )
